3ac70614f7c856054e84d893ed1a915cfa397655
jasmin <cjm98@smartel.co.kr>
2026-01-09 14:37:27 +0900

    fix : CH2 음의 정수입력 시 예외 발생로직 추가 [최재민]
    음의 정수를 입력시 정상적으로 데이털가 할당되는 현상을 조건문과 예외발생(프로그램 종료) 활용하여 수정

--- src/Chapter1/App.java
+++ src/Chapter1/App.java
@@ -3,15 +3,24 @@ package Chapter1;
 import java.util.Scanner;
 
 public class App {
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
         System.out.println("첫 번째 양의 정수를 입력하세요.");
         int a = input.nextInt();
+
+        if (a < 0) {
+            throw new ArithmeticException("양의 정수만 입력 할 수 있습니다 입력값 " + a +"는 음수입니다.");
+        }
         System.out.println("첫 번째 양의 정수:" + a);
 
         System.out.println("두 번째 양의 정수를 입력하세요.");
         int b = input.nextInt();
+
+        if (b < 0) {
+            throw new ArithmeticException("양의 정수만 입력 할 수 있습니다 입력값 " + b +"는 음수입니다.");
+        }
         System.out.println("두 번째 양의 정수:" + b);
+
     }
 }
