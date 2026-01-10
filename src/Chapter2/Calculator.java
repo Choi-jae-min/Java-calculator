b7b426ea2900b7d3d299f7b01bd036c93f12362c
jasmin <a01095567997@gmail.com>
2026-01-10 20:07:11 +0900

    fix : 기존 래퍼클래스 Integer를 컬랙션으로 변경 [Chapter2][최재민]

--- src/Chapter2/Calculator.java
+++ src/Chapter2/Calculator.java
@@ -1,19 +1,18 @@
 package Chapter2;
 
+import java.util.ArrayList;
+
 public class Calculator {
 
-    // 컬랙션 타입 Integer 생성.
-    Integer calculateResult;
+    // 래퍼 클래스가 아닌 진짜 컬랙션 타입 생성
+    ArrayList<Integer> calculateResult = new ArrayList<>();
 
-    // Chapter1 에서 사용한 향상된 switch (lamda) 를 사용하여 두 양의 정수를 매개변수로 받아 사칙 연산을 진행하는 매서드 작성.
     int CalculateNumber(int positiveNumber1, int positiveNumber2 , char operator) {
-
-        // 양의 정수만 입력 받을 수 있도록 변경
         if (positiveNumber1 < 0 || positiveNumber2 < 0) {
             throw new IllegalArgumentException("양의 정수만 입력 할 수 있습니다 첫번째 입력값" + positiveNumber1 + ", 두번째 입력값" +positiveNumber2);
         }
-
-        this.calculateResult = switch (operator) {
+        // 변수에 저장 하지 않고 오직 계산 결과만 반환 하도록 변경 , 저장은 다른 매서드로.
+        return switch (operator) {
             case '+' -> positiveNumber1 + positiveNumber2;
             case '-' -> positiveNumber1 - positiveNumber2;
             case '*' -> positiveNumber1 * positiveNumber2;
@@ -25,8 +24,5 @@ public class Calculator {
             }
             default -> throw new IllegalArgumentException("올바른 사칙연산 기호를 입력하세요 현재 입력값 : " + operator);
         };
-
-        return calculateResult;
     }
-
 }
