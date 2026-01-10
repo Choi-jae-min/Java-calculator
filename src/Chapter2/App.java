33f42368a001cb47a07f0601a2277d40a9752172
jasmin <cjm98@smartel.co.kr>
2026-01-10 20:33:33 +0900

    feat : main class에서 claculator class를 이용해 계산 결과 저장 [chapter2][최재민]

--- src/Chapter2/App.java
+++ src/Chapter2/App.java
@@ -1,7 +1,35 @@
 package Chapter2;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
+        Calculator calculator = new Calculator();
 
+        String isExitString = "";
+        // chapter1 에서 반복문 안쪽에 있었는데 반복할때마다
+        // Scanner 객체가 계속 생성 되기 때문에 바깥으로 빼줌
+        Scanner input = new Scanner(System.in);
+
+        while (!isExitString.equals("exit")) {
+
+            System.out.println("첫 번째 양의 정수를 입력하세요.");
+            int positiveNumber1 = input.nextInt();
+
+            System.out.println("두 번째 양의 정수를 입력하세요.");
+            int positiveNumber2 = input.nextInt();
+
+            System.out.println("사칙연산 기호를 입력하세요 + , - , * , / ");
+            char operator = input.next().charAt(0);
+
+            int result = calculator.CalculateNumber(positiveNumber1, positiveNumber2, operator);
+
+            calculator.calculateResult.add(result);
+
+            System.out.println("지금까지 계산 결과 : " + calculator.calculateResult);
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            isExitString = input.next();
+        }
     }
 }
