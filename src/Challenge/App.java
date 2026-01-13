b1bf350ac0238271f49bd3672242568acecb5a80
jasmin <cjm98@smartel.co.kr>
2026-01-13 16:04:10 +0900

    feat : 메뉴를 출력하여 사용자가 원하는 기능을 바로 사용할 수 있게 변경 [advancement][최재민]

--- src/Challenge/App.java
+++ src/Challenge/App.java
@@ -5,47 +5,54 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        // 타입을 제네릭으로 선언
         Calculator<Double> calculator = new Calculator<>();
 
-        String isExitString = "";
-        // chapter1 에서 반복문 안쪽에 있었는데 반복할때마다
-        // Scanner 객체가 계속 생성 되기 때문에 바깥으로 빼줌
+        String menu = "";
         Scanner input = new Scanner(System.in);
-        //기존 int 로 받던 스캐너와 result 타입을 변경 해 주었습니다.
-        while (!isExitString.equals("exit")) {
+        while (!menu.equals("exit")) {
+            System.out.println("원하시는 메뉴를 선택 해 주세요.");
+            System.out.println("1 : 계산기 | 2 : 가장 오래된 기록 삭제 | 3 : 더 큰 숫자 찾기 | exit 입력시 종료");
+            menu = input.next();
 
-            System.out.println("첫 번째 양의 실수를 입력하세요.");
-            double positiveNumber1 = input.nextDouble();
-            calculator.checkValidNumber(positiveNumber1);
+            switch (menu) {
+                case "1":
+                    System.out.println("첫 번째 양의 실수를 입력하세요.");
+                    double positiveNumber1 = input.nextDouble();
+                    calculator.checkValidNumber(positiveNumber1);
 
-            System.out.println("두 번째 양의 실수를 입력하세요.");
-            double positiveNumber2 = input.nextDouble();
-            calculator.checkValidNumber(positiveNumber2);
+                    System.out.println("두 번째 양의 실수를 입력하세요.");
+                    double positiveNumber2 = input.nextDouble();
+                    calculator.checkValidNumber(positiveNumber2);
 
-            System.out.println("사칙연산 기호를 입력하세요 + , - , * , / ");
-            char operator = input.next().charAt(0);
-            OperatorType operatorType = OperatorType.setSymbol(operator);
-            Double result = calculator.CalculateNumber(positiveNumber1, positiveNumber2, operatorType);
+                    System.out.println("사칙연산 기호를 입력하세요 + , - , * , / ");
+                    char operator = input.next().charAt(0);
+                    OperatorType operatorType = OperatorType.setSymbol(operator);
+                    Double result = calculator.CalculateNumber(positiveNumber1, positiveNumber2, operatorType);
 
-            calculator.addCalculateResult(result);
-
-            System.out.println("지금까지 계산 결과 : " + calculator.getCalculateResult());
-
-            // 입력 값보다 큰 결과들을 반환 할 수 있는 메서드 추가
-            System.out.println("비교할 정수 입력");
-            double comparisonNumber = input.nextDouble();
-            ArrayList<Double> List = calculator.findResultsGreaterThan(comparisonNumber);
-            System.out.println(comparisonNumber +" 보다 큰 결과 리스트 :" + List);
-
-            System.out.println("가장 첫번째 계산 결과를 삭제 하시겠습니까? D입력시 삭제");
-            char doDelete = input.next().charAt(0);
-            if (doDelete == 'd') {
-                calculator.removeFristCalculateResult();
+                    calculator.addCalculateResult(result);
+                    System.out.println("지금까지 계산 결과 : " + calculator.getCalculateResult());
+                    break;
+                case "2":
+                    // 한번 더 검증
+                    System.out.println("가장 첫번째 계산 결과를 삭제 하시겠습니까? D입력시 삭제");
+                    char doDelete = input.next().charAt(0);
+                    if (doDelete == 'd') {
+                        calculator.removeFristCalculateResult();
+                    }
+                    break;
+                case "3":
+                    System.out.println("비교할 정수 입력");
+                    double comparisonNumber = input.nextDouble();
+                    ArrayList<Double> List = calculator.findResultsGreaterThan(comparisonNumber);
+                    System.out.println(comparisonNumber +" 보다 큰 결과 리스트 :" + List);
+                    break;
+                case "exit":
+                    System.out.println("프로그램을 종료합니다..");
+                    break;
+                default:
+                    System.out.println("잘못된 메뉴를 입력 하셨습니다.");
+                    break;
             }
-
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            isExitString = input.next();
         }
     }
 }
